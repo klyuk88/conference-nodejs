ac0ecc78abe2b3a0b183db1dc82be4d54b49460d
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2022-02-14 10:14:51 +0300

    56576

--- routes/conditions.js
+++ routes/conditions.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import fetch from 'node-fetch'
+import Showdown from 'showdown'
+
+const converter = new Showdown.Converter()
 const router = express.Router()
 
 /* GET home page. */
@@ -14,6 +17,8 @@ router.get('/', function(req, res, next) {
     const liveContract = pageData.live_contract.data ? process.env.ADMIN_URL + pageData.live_contract.data.attributes.url : '#'
     const liveCheck = pageData.live_check.data ? process.env.ADMIN_URL + pageData.live_check.data.attributes.url : '#'
     const publicationContract = pageData.publication_contract.data ? process.env.ADMIN_URL + pageData.publication_contract.data.attributes.url : '#'
+    const afterTableText = converter.makeHtml(pageData.after_table_text)
+    const beforeTableText = converter.makeHtml(pageData.before_table_text)
     res.render('conditions', {
       metaTitle: 'Условия участия',
       pageData,
@@ -21,7 +26,9 @@ router.get('/', function(req, res, next) {
       registrationContract,
       liveContract,
       liveCheck,
-      publicationContract
+      publicationContract,
+      afterTableText,
+      beforeTableText
 
     });
   })
